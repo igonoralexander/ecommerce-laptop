506fc937331bf38fca5e2e5136e0b711d1cfd075
igonoralexander <lexphoenix59@gmail.com>
2025-03-04 16:16:19 +0100

    Changes to Web

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
